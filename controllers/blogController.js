ec9c065f1fa3e69367b1d60a0807077e9e71db6a
NewChakrit <imnew108@gmail.com>
2022-05-18 02:31:48 -0700

    'save'

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,3 +1,25 @@
+const slugify = require("slugify");
+const Blogs = require("../models/blogs");
+
 exports.create = (req, res) => {
-  res.json({ data: "Hello from blog controller" });
+  const { title, content, author } = req.body;
+  const slug = slugify(title);
+
+  // validate
+  switch (true) {
+    case !title:
+      return res.status(400).json({ error: "Please input your title." });
+      break;
+    case !content:
+      return res.status(400).json({ error: "Please input your content." });
+      break;
+  }
+
+  // Submit data
+  Blogs.create({ title, content, author, slug }, (err, blog) => {
+    if (err) {
+      res.status(400).json({ error: "Can't save duplicate article titles." });
+    }
+    res.json(blog);
+  });
 };
--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { create } = require("../controllers/blogController");
 
-router.get("/blog", create);
+router.post("/create", create);
 
 module.exports = router;
